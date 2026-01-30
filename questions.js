1cc021b949fcb9570c03c2613a149d9b5b4dcd58
ezvinet <139599257+ezvinet@users.noreply.github.com>
2026-01-30 13:44:23 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -6,7 +6,7 @@ const examPapers = {
         questions: [
             {
                 q: "학교에 갑니다",
-                o: [ { text: "선택 1", audio: "audio/track_001.MP3" },"회사","시장","집"],
+                o: [ { text: "선택 1", audio: "track_001.MP3" },"회사","시장","집"],
                 a: 0,
                 si: "පාසලට යනවා",
                 audio: "audio/track_001.MP3",
